07c5b958dafddc82e1c6e961741a50899aa17de8
thinking-weed <133303560+thinking-weed@users.noreply.github.com>
2025-01-27 23:18:54 +0900

    QuerySetをいじる

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('all_users/', accounts_views.users_index, name='all_users'),
     path('checked_users/', accounts_views.checked_index, name='checked_index'),
     path('all_users_list/' ,accounts_views.users_index_list, name='users_index_list'),
-    path('all_users_values/' ,accounts_views.users_index_values, name='users_index_values')
+    path('all_users_values/' ,accounts_views.users_index_values, name='users_index_values'),
+    path('all_users_values_list/' ,accounts_views.users_index_values_list, name='users_index_values_list'),
+    path('first_last_counts_get/' , accounts_views.first_last_counts_get, name='first_last_counts_get'),
+    path('customize_queryset/' ,accounts_views.customize_queryset, name='customize_queryset')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from accounts.models import User
 from accounts.forms import CheckedUserForm
-from django_portfolio import settings
+from django.db.models import QuerySet 
+#Djangoでall()を使ったときに取り出されるクエリ取得用にいろいろ機能拡張したセット
 
 #prefixのURLは「accounts/」
 
@@ -47,3 +48,47 @@ def users_index_values(request):
         'userdatas':userdatas
     }
     return render(request, 'accounts/all_users_values.html', params)
+
+def users_index_values_list(request):
+    userdatas = User.objects.all().order_by('id').values_list('id','username','email')
+    params = { #.values_listで取り出したモデルをリストとして取り出す
+        'title':'Hello',
+        'userdatas':userdatas
+    }
+    return render(request, 'accounts/all_users_values.html', params)
+
+def first_last_counts_get(request):
+    first = User.objects.all().first() #allなどで得られたレコードの内、最初のものだけ返す
+    last = User.objects.all().last() #allなどで得られたレコードの内、最後のものだけ返す
+    num = User.objects.all().count() #取得したレコード数を返す
+    # 特定のカラムの値を取得
+    first_name = first.username if first else None  # 'name' フィールドの値（存在しない場合はNone）
+    last_email = last.email if last else None  # 'email' フィールドの値（存在しない場合はNone）
+
+    userdatas = [num, first_name, last_email]
+
+    params = {
+        'title':'Hello',
+        'userdatas':userdatas
+    }
+    return render(request, 'accounts/all_users_values.html', params)
+
+#Querysetの機能の一部書き換え
+def __new__str__(self):
+    result = ''
+    for item in self:
+        result += '<tr>'
+        for k in item:
+            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+        result += '</tr>'
+    return result
+
+QuerySet.__str__ = __new__str__ #左辺の__str__はmodels.pyで定義しているものと思われる
+
+def customize_queryset(request):
+    userdata = User.objects.all().order_by('id').values('id','username','email')
+    params = { #.values_listで取り出したモデルをリストとして取り出す
+        'title':'Hello',
+        'userdata':userdata
+    }
+    return render(request, 'accounts/customize_queryset.html', params)
