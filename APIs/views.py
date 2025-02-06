7115f2beace8284eaea4eb4727b325a2ac3cea22
thinking-weed <133303560+thinking-weed@users.noreply.github.com>
2025-02-06 23:08:55 +0900

    JSONRESPONSEの配列にコメントをつけやすいように改行

--- APIs/views.py
+++ APIs/views.py
@@ -1,12 +1,16 @@
 from django.http import JsonResponse
-from APIs.functions import get_address_from_zipcode  # 別ファイルの関数をインポート
+# 別ファイルの関数をインポート
+from APIs.functions import get_address_from_zipcode
 
 def postcode_search(request):
     """郵便番号を元に住所を検索するAPIビュー"""
     
     zip_code = request.GET.get("zipcode")
     if not zip_code:
-        return JsonResponse({"error": "郵便番号を指定してください"}, status=400)
+        return JsonResponse(
+                            {"error": "郵便番号を指定してください"},
+                            status=400
+                            )
 
     response_data = get_address_from_zipcode(zip_code)
     return JsonResponse(response_data,
